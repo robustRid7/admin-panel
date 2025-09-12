b38f82bfd5eaadd341a7e18afe493392ab62a3b8
robustRid7 <rhythm.dholliya.07@gmail.com>
2025-09-12 14:37:39 +0530

    dashboard count changes

--- src/dto/dashBoard.dto.js
+++ src/dto/dashBoard.dto.js
@@ -1,16 +1,17 @@
 const Joi = require("joi");
 
 const getCampaignListCountSchema = Joi.object({
-  campaignId: Joi.string().hex().length(24).optional(), // ObjectId
-  from: Joi.date().iso().optional(), // ISO date string
-  to: Joi.date().iso().optional(),   // ISO date string
+  campaignId: Joi.string().hex().length(24).optional().allow(null), // ObjectId or null
+  from: Joi.date().iso().optional().allow(null), // ISO date string or null
+  to: Joi.date().iso().optional().allow(null),   // ISO date string or null
 });
 
 const getCampaigChartSchema = Joi.object({
-  campaignId: Joi.string().hex().length(24).optional(), // ObjectId
-  from: Joi.date().iso().optional(), // ISO date string
-  to: Joi.date().iso().optional(),   // ISO date string
+  campaignId: Joi.string().hex().length(24).optional().allow(null), // ObjectId or null
+  from: Joi.date().iso().optional().allow(null), // ISO date string or null
+  to: Joi.date().iso().optional().allow(null),   // ISO date string or null
 });
+
 
 module.exports = {
   getCampaignListCountSchema,
